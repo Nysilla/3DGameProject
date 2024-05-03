aeb8017d2a089247913f8802df658d836bc2e10b
MarshmallowBoy <traysoncazier719@gmail.com>
2024-05-03 11:50:40 -0700

    Added cost marker

--- 3DGame/Assets/Scripts/Spawner.cs
+++ 3DGame/Assets/Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Spawner1 : MonoBehaviour
@@ -9,6 +10,7 @@ public class Spawner1 : MonoBehaviour
     public int[] Prices;
     public int ActiveTower;
     public GameObject ActiveInstantiate;
+    public TextMeshProUGUI MoneyText;
 
     void Update()
     {
@@ -37,9 +39,16 @@ public class Spawner1 : MonoBehaviour
             if (Input.GetButtonDown("Fire2") && GetComponentInParent<PlayerHealth>().Money - Prices[ActiveTower] >= 0)
             {
                 Destroy(ActiveInstantiate);
+                MoneyText.text = "-$" + Prices[ActiveTower].ToString();
+                Invoke("MakeTextNullAgain", 2);
                 GetComponentInParent<PlayerHealth>().Money -= Prices[ActiveTower];
                 Instantiate(Prefabs[ActiveTower], hit.point, Quaternion.identity);
             }
         }
+    }
+
+    void MakeTextNullAgain()
+    {
+        MoneyText.text = "";
     }
 }
